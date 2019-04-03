81cdd3590fa33c5928f0b3eed2ca967c8a399835
Linh Phung <phung.linh2010@gmail.com>
2019-04-03 23:08:44 +0200

    Initial data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Invoice } from '../models/invoice.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  invoices: Invoice[];
+  index = 0;
+  customerNameList: string[]
+
   constructor() { }
+
+  fetchAllCustomerNames() {
+    this.invoices.map(
+      invoce => invoce.customer_name
+    )
+  }
+
+  import() {
+    let dataString = prompt('Bitte JSON als String angeben');
+    this.invoices = JSON.parse(dataString);
+    const customerNames = this.fetchAllCustomerNames();
+    console.log(customerNames)
+  }
+
+  export() {
+    console.log(this.invoices)
+  }
 }
